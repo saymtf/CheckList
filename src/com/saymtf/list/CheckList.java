fe3c841ebb330bb97cd0b7c4c399addd1c7f6c38
Thane <mtfenton1991@gmail.com>
2016-01-05 14:05:11 -0800

    Updated CheckList

--- src/com/saymtf/list/CheckList.java
+++ src/com/saymtf/list/CheckList.java
@@ -24,12 +24,16 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 public class CheckList{
 	public static void main(String[] args) {
 		new CheckList();
 	}
+
+
 	private JFrame frame;
 	private JPanel editPanel;
 	private JPanel cardPanel;
@@ -46,9 +50,8 @@ public class CheckList{
 		cardPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		cardPanel.setLayout(new CardLayout());
 
-		
 		JPanel mainPanel = new JPanel(); // main panel
-		
+
 		// Settings Button
 		JButton editButton = new JButton(new ImageIcon("/Users/mitchellfenton/Documents/workspace/RollicksCheckList/images/setting.png"));
 
@@ -56,12 +59,12 @@ public class CheckList{
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JPasswordField passwordField = new JPasswordField(); // password field
+				//JPasswordField passwordField = new JPasswordField(); // password field
 				//JOptionPane.showMessageDialog(null, passwordField, "Password", JOptionPane.WARNING_MESSAGE); // show dialog
-				//if(passwordField.getPassword().equals("poop")) {
-					CardLayout cardLayout = (CardLayout) cardPanel.getLayout();
-					cardLayout.show(cardPanel, "EditPanel");
-					changeFrame();
+				//if(new String(passwordField.getPassword()).equals("poop")) {
+				CardLayout cardLayout = (CardLayout) cardPanel.getLayout();
+				cardLayout.show(cardPanel, "EditPanel");
+				changeFrame();
 				//}
 
 			}
@@ -74,7 +77,7 @@ public class CheckList{
 		mainPanel.add(editButton);
 		cardPanel.add(mainPanel, "MainPanel");
 		cardPanel.add(editPanel, "EditPanel");
-		
+
 		frame.add(cardPanel);
 		//These are needed
 		frame.setSize(new Dimension(1024, 1024));
@@ -82,74 +85,88 @@ public class CheckList{
 		frame.setVisible(true);
 		//frame.pack();
 	}
-	
+
 	private void changeFrame() {
-		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new GridLayout(0,1,5,5));
+		JPanel mainPanel = new JPanel(); // Main Panel for Settings
+		mainPanel.setLayout(new GridLayout(0,1,5,5)); // make it a grid layout
+
+		// Add New Description Button
 		JButton addButton = new JButton("+");
-		addButton.setPreferredSize(new Dimension(128,72));
-		
-		JScrollBar scrollBar = new JScrollBar(JScrollBar.VERTICAL);
-		JPanel scrollPanel = new JPanel();
-		
-		scrollBar.add(scrollPanel);
-		// Display All the JPanel Texts
-		displayPanels(mainPanel);
-		
+		addButton.setPreferredSize(new Dimension(128,16));
+
+		//Description Scroll Panel
+		JPanel scrollPanel = new JPanel(new GridLayout(0,1,5,5));
+
+		//Set the Scroll Pane with Scroll Panel Component
+		JScrollPane scrollPane = new JScrollPane(scrollPanel);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPanel.setAutoscrolls(true);
+		scrollPane.setPreferredSize(new Dimension(800,300));
+
+		// Display All the Current JPanel Texts
+		displayPanels(scrollPanel);
+
+		//Add to Main Panel
+
+
+		//When Add Button Gets Clicked..
 		addButton.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JPanel panel = new JPanel(); // create panel to add to list
-				panel.setPreferredSize(new Dimension(1024,512));
-				panel.setBackground(new Color(100,100,100));
+				JPanel panel = new JPanel(); // create panel to add to list 
 				JTextArea textArea = new JTextArea("");
-				JTextField sign = new JTextField("");
-				textArea.setEditable(true);
-				sign.setEditable(true);
+				textArea.setPreferredSize(new Dimension(256,48));
+				JTextArea sign = new JTextArea("");
+				sign.setPreferredSize(new Dimension(72,48));  
 				panel.add(textArea);
 				panel.add(sign);
-				
-				// Add Panels to list
+
+				// Add Panel to List
 				panels.add(panel);
-				
+
+
 				//Display the panels in the list
 				displayPanels(scrollPanel);
-				
-				scrollPanel.validate();
+				scrollPane.validate();
+
 			}
 		});
-		
-		
+
+		// Finish Editing Button
 		JButton doneEdittingButton = new JButton("Done");
-		doneEdittingButton.setPreferredSize(new Dimension(128,72));
+		doneEdittingButton.setPreferredSize(new Dimension(128,16));
 		
+		//When Button is Clicked
 		doneEdittingButton.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// iterate through and make setEditable False!
-				
-				
-				// Go Back
+
+
+				// Go Back To Main Layout
 				CardLayout cardLayout = (CardLayout) cardPanel.getLayout();
 				cardLayout.show(cardPanel, "MainPanel");
 				editPanel.remove(mainPanel);
 			}
 		});
+
 		
+		// Add Components to Main Panel
 		mainPanel.add(addButton);
-		mainPanel.add(scrollBar);
+		mainPanel.add(scrollPane);
 		mainPanel.add(doneEdittingButton);
-		mainPanel.setPreferredSize(new Dimension(512,512));
+
+		// Add To Edit Panel
 		editPanel.add(mainPanel);
 	}
-	
+
 	private void displayPanels(JPanel textAreaPanels) {
 		for(JPanel panel: panels) {
 			textAreaPanels.add(panel);
 		}
-		
+
 	}
-	
+
 }
